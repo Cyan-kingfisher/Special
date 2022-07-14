893d2e670d581f76549cece436cf4c832861935e
cyan.kingfisher <cyan.kingfisher@foxmail.com>
2022-07-14 18:16:48 +0800

    并发编程场景五: 使用Future、线程池实现异步编程，异步任务有返回信息

---
+++ Concurrency/src/main/java/scene/five/Main.java
@@ -0,0 +1,34 @@
+package scene.five;
+
+import common.Something;
+import common.ThreadPoolFactory;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 场景4：使用Future、线程池实现异步编程，异步任务有返回信息
+ *
+ * @author cyan
+ * @since 2022/7/14
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Something something = new Something();
+        ThreadPoolExecutor threadPoolExecutor = ThreadPoolFactory.create();
+
+        long start = System.currentTimeMillis();
+
+        Future<String> taskC = threadPoolExecutor.submit(something::doSomethingC);
+
+        String resultTaskD = something.doSomethingD();
+        String resultTaskC = taskC.get();
+
+        long end = System.currentTimeMillis();
+        System.out.println("Run time: " + (end-start));
+        System.out.println("Run result: " + resultTaskC + ", " + resultTaskD);
+    }
+
+}
