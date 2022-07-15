608fa943768855803b2b23eb59344ffcd207193e
cyan.kingfisher <cyan.kingfisher@foxmail.com>
2022-07-15 22:53:19 +0800

    并发编程场景八: 基于CompletableFuture.runAsync实现有返回值的异步计算

---
+++ Concurrency/src/main/java/nine/Main.java
@@ -0,0 +1,37 @@
+package nine;
+
+import common.Something;
+import common.ThreadPoolFactory;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 场景9： 基于CompletableFuture.supplyAsync实现有返回值的异步计算
+ *
+ * @author cyan
+ * @since 2022/7/15
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Something something = new Something();
+        ThreadPoolExecutor threadPoolExecutor = ThreadPoolFactory.create();
+
+        long start = System.currentTimeMillis();
+
+        /**
+         * 默认线程池为ForkJoinPool
+         * CompletableFuture completableFuture = CompletableFuture.runAsync(something::doSomethingC);
+         */
+        CompletableFuture completableFuture = CompletableFuture.supplyAsync(something::doSomethingC, threadPoolExecutor);
+        String resultTaskD = something.doSomethingD();
+        System.out.println("thread run result: " + completableFuture.get() + ", main thread result: " + resultTaskD);
+
+        long end = System.currentTimeMillis();
+        System.out.println("Run time：" + (end - start));
+
+    }
+
+}
