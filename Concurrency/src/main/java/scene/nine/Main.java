35cf2c6446f2331592d56dddad50f2d2053d5d97
cyan.kingfisher <cyan.kingfisher@foxmail.com>
2022-07-17 10:15:58 +0800

    并发编程场景十: 基于thenAccept和thenAcceptAsync实现异步任务A，执行完毕后激活任务B，任务B获取任务A的返回值

--- Concurrency/src/main/java/scene/nine/Main.java
+++ Concurrency/src/main/java/scene/nine/Main.java
@@ -7,8 +7,8 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadPoolExecutor;
 
 /**
- * 场景9： 基于thenRun和thenRunAsync实现异步任务A，执行完毕后激活任务B
- * 任务B获取不到任务A的结果
+ * 场景9： 基于CompletableFuture.thenRun和CompletableFuture.thenRunAsync实现异步任务A，执行完毕后激活任务B
+ * 任务B获取不到任务A的结果，主线程获取不到任务B的返回值
  * thenRun：使用ForkJoinPool线程池，任务A和任务B使用同一线程
  * thenRunAsync(Runnable action, Executor executor)：使用自定义线程，任务A使用ForkJoinPool线程和任务B使用自定义线程池线程
  *
---
+++ Concurrency/src/main/java/scene/ten/Main.java
@@ -0,0 +1,39 @@
+package scene.ten;
+
+import common.Something;
+import common.ThreadPoolFactory;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 场景10： 基于CompletableFuture.thenAccept和CompletableFuture.thenAcceptAsync实现异步任务A，执行完毕后激活任务B
+ * 任务B获取能任务A的结果，主线程获取不到任务B的返回值
+ * thenAccept：使用ForkJoinPool线程池，任务A和任务B使用同一线程
+ * thenAcceptAsync(Runnable action, Executor executor)：使用自定义线程，任务A使用ForkJoinPool线程和任务B使用自定义线程池线程
+ *
+ * @author cyan
+ * @since 2022/7/16
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Something something = new Something();
+        ThreadPoolExecutor threadPoolExecutor = ThreadPoolFactory.create();
+
+        long start = System.currentTimeMillis();
+
+        CompletableFuture<String> completableFutureTaskC = CompletableFuture.supplyAsync(something::doSomethingC);
+        CompletableFuture<Void> completableFutureTaskD = completableFutureTaskC.thenAccept(
+                res -> {
+                    System.out.println("TaskD 获取到 TaskC 的结果: " + res);
+                    something.doSomethingD();
+                }
+        );
+        long end = System.currentTimeMillis();
+        completableFutureTaskD.get();
+        System.out.println("Run time：" + (end - start));
+    }
+
+}
